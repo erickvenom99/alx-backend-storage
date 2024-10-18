5872e3fdb2c0b6a13892cf14c6672a2940ff63a1
erickvenom99 <prevenakhtar@mail.com>
2024-10-18 04:42:28 +0000

    update task1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,5 @@
 -- Verify if the table exists before creating it
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255),
